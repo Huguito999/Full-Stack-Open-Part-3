498f1ee5adf17b54be559d365bdad514e18e26ab
Huguito999 <123948059+Huguito999@users.noreply.github.com>
2024-07-11 16:04:22 +0200

    part 3

---
+++ part3/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+morgan.token('body', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let phonebook = [
+  {
+    "id": 1,
+    "name": "Ramon",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+app.get('/', (request, response) => {
+  response.send('<h1>hello world</h1>')
+})
+app.get('/api/persons', (request, response) => {
+  response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = phonebook.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  const numberOfPeople = phonebook.length;
+
+  const localDate = new Date();
+  const formatedDate = localDate.toLocaleString();
+
+  response.send(`<p>Phonebook has info for ${numberOfPeople} people</p><p>${formatedDate}</p>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(person => person.id !== id)
+  response.status(204).end()
+
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number missing'
+    });
+  }
+  const duplicatePerson = phonebook.find(person => person.name === body.name);
+  if (duplicatePerson) {
+    return response.status(400).json({
+      error: 'Name must be unique'
+    });
+  }
+
+  const generateId = () => {
+    return Math.floor(Math.random() * 1e12);
+  };
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  phonebook = phonebook.concat(person);
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
